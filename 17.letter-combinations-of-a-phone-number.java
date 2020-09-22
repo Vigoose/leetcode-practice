83988513177807d9746158bdf27ef4a8c39ccca9
unknown <vigoose@gmail.com>
2020-09-21 21:26:40 -0700

    Second one

--- 17.letter-combinations-of-a-phone-number.java
+++ 17.letter-combinations-of-a-phone-number.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * @lc app=leetcode id=17 lang=java
  *
@@ -33,12 +36,44 @@
  * Although the above answer is in lexicographical order, your answer could be
  * in any order you want.
  * 
+ * 
+ * 2(abc)3(def)
+ * 
+ * 
+ *0       a            b              c
+ *1   ad  ae af     bd be bf      cd  ce  cf
+ * 
  */
 
+
+
 // @lc code=start
+
+
 class Solution {
+    String[] nums = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
-        
+        List<String> res = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return res;
+        }
+        dfs(res, digits, 0, new StringBuilder());
+        return res;
+    }
+
+    private void dfs(List<String> res, String digits, int index, StringBuilder sb) {
+        if (index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+
+        int digit = Character.getNumericValue(digits.charAt(index));
+        String numbers = nums[digit];
+        for (int i = 0; i < numbers.length(); i++) {
+            sb.append(numbers.charAt(i));
+            dfs(res, digits, index + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
 // @lc code=end
